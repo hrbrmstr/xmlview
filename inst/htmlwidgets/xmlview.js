b2fdf65838720040230385e3ee944a4dd030b4b0
Bob Rudis <bob@rudis.net>
2016-01-12 20:22:38 -0500

    better err msg

--- inst/htmlwidgets/xmlview.js
+++ inst/htmlwidgets/xmlview.js
@@ -20,16 +20,18 @@ HTMLWidgets.widget({
 
     document.getElementsByTagName("head")[0].appendChild(link);
 
+    var msg = "<div style='max-width:100%; width:100%; font-family:monospace; display:none; border: 1px solid; margin:0; margin-top:5px; padding:5px; color: #9f6000; background-color: #feefb3;' id='msg'></div>";
+
     var filter = "";
 
     if (param.addFilter) {
-      filter = "XPath: <input style='outline: none;' type='text' size=80 id='xpath' onchange='filter_xpath();'/> <button style='border: none; outline:0; background-color:#ffffff;' onclick='reset_form();'><img alt='Reset' width='20px' src='lib/vkbeautify-0.99.00/./reset.png'/></button><br/>";
+      filter = "<span style='font-family:monospace; font-weight:bold'>XPath:</span> <input style='font-family: monospace; outline: none;' type='text' size=69 id='xpath' onchange='filter_xpath();'/> <button style='border: none; outline:0; background-color:#ffffff;' onclick='reset_form();'><img alt='Reset' width='20px' src='lib/vkbeautify-0.99.00/./reset.png'/></button><br/>";
     }
 
-    el.innerHTML = filter + "<pre><code class='html' id='xmldiv'></code></pre>";
+    el.innerHTML = filter + msg + "<pre><code class='html' id='xmldiv'></code></pre>";
 
     var xml_div = document.getElementById('xmldiv');
-    xml_div.innerText =  vkbeautify.xml(param.xmlDoc);
+    xml_div.innerText = vkbeautify.xml(param.xmlDoc);
 
     hljs.initHighlighting() ;
     hljs.highlightBlock(document.getElementById('xmldiv'));
@@ -42,30 +44,47 @@ HTMLWidgets.widget({
 
 function filter_xpath() {
 
+  document.getElementById('msg').style.display = "none";
+
   var xpath = document.getElementById("xpath").value.trim();
-  var results = glob.parsedOrig.evaluate(xpath,
-                                 glob.parsedOrig,
-                                 null,
-                                 XPathResult.UNORDERED_NODE_ITERATOR_TYPE,
-                                 null);
   var out_xml = "" ;
-  var res = results.iterateNext() ;
 
-  while(res) {
-    out_xml = out_xml + res.outerHTML ;
-    res = results.iterateNext();
+  try {
+    var results =
+      glob.parsedOrig.evaluate(xpath,
+                       glob.parsedOrig,
+                       null,
+                       XPathResult.UNORDERED_NODE_ITERATOR_TYPE,
+                       null);
+
+    var res = results.iterateNext() ;
+
+    while(res) {
+      out_xml = out_xml + res.outerHTML;
+      res = results.iterateNext();
+    }
+
+    var xml_div = document.getElementById('xmldiv');
+    xml_div.innerText = vkbeautify.xml(out_xml);
+
+    hljs.highlightBlock(document.getElementById('xmldiv'));
+
+  } catch(err) {
+
+    glob.err = err;
+
+    document.getElementById('msg').innerText = "Invalid XPath";
+    document.getElementById('msg').style.display = "inline-block";
+
   }
-
-  var xml_div = document.getElementById('xmldiv');
-  xml_div.innerText = vkbeautify.xml(out_xml);
-
-  hljs.highlightBlock(document.getElementById('xmldiv'));
 
 }
 
 function reset_form(doc) {
 
+  document.getElementById('msg').style.display = "none";
   document.getElementById('xpath').value = "" ;
+
   var xml_div = document.getElementById('xmldiv');
   xml_div.innerText =  vkbeautify.xml(glob.xmlDoc);
   hljs.highlightBlock(document.getElementById('xmldiv'));
