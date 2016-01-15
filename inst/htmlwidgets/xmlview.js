8960e8e425815962ac44c3ca7206b03fabf1ddd0
Bob Rudis <bob@rudis.net>
2016-01-14 23:37:16 -0500

    better refactoring

--- inst/htmlwidgets/xmlview.js
+++ inst/htmlwidgets/xmlview.js
@@ -7,7 +7,6 @@ HTMLWidgets.widget({
 
   name: 'xmlview',
   type: 'output',
-  thel: {},
 
   initialize: function(el, width, height) { return {} },
 
@@ -15,21 +14,15 @@ HTMLWidgets.widget({
 
     var self = this;
 
-    this.thel = el;
-
-    if (typeof(glob) === "undefined") { glob = {} }
+    self.thel = el;
+    self.glob = {} ;
 
     function filter_xpath() {
-      var msg = $qs("div#" + self.thel.id + " > div.msgdiv");
-      var xpath = $qs("div#" + self.thel.id + " > div.filterdiv > input.filterinput").value;
-      msg.style.display = "none";
-      do_filter(xpath.trim());
+      self.msg.style.display = "none";
+      do_filter(self.xpath.value.trim());
     }
 
     function do_filter(xpath) {
-
-      var rcode = $qs("div#" + self.thel.id + " > div.rcodediv");
-      var msg = $qs("div#" + self.thel.id + " > div.msgdiv");
 
       if (xpath === "") return(reset_form());
 
@@ -37,12 +30,12 @@ HTMLWidgets.widget({
 
       try {
 
-        var nsResolver = glob[self.thel.id].parsedOrig.createNSResolver(
-                                    glob[self.thel.id].parsedOrig);
+        var nsResolver = self.glob.parsedOrig.createNSResolver(
+                                    self.glob.parsedOrig);
 
         var results =
-          glob[self.thel.id].parsedOrig.evaluate(xpath,
-                           glob[self.thel.id].parsedOrig,
+          self.glob.parsedOrig.evaluate(xpath,
+                           self.glob.parsedOrig,
                            nsResolver,
                            XPathResult.UNORDERED_NODE_ITERATOR_TYPE,
                            null);
@@ -50,23 +43,22 @@ HTMLWidgets.widget({
         var res = results.iterateNext() ;
 
         while(res) {
-          out_xml = out_xml + glob[self.thel.id].serialize.serializeToString(res);
+          out_xml = out_xml + self.glob.serialize.serializeToString(res);
           res = results.iterateNext();
         }
 
-        var xml_div = $qs("div#" + el.id + " > pre > code.xmldiv");
-        xml_div.innerText = vkbeautify.xml(out_xml);
-        xml_div.textContent = vkbeautify.xml(out_xml);
+        self.xml_div.innerText = vkbeautify.xml(out_xml);
+        self.xml_div.textContent = vkbeautify.xml(out_xml);
 
-        hljs.highlightBlock(xml_div);
+        hljs.highlightBlock(self.xml_div);
 
       } catch(err) {
 
-        rcode.style.display = "none";
+        self.rcode.style.display = "none";
 
-        msg.innerText = "Invalid XPath";
-        msg.textContent = "Invalid XPath";
-        msg.style.display = "inline-block";
+        self.msg.innerText = "Invalid XPath";
+        self.msg.textContent = "Invalid XPath";
+        self.msg.style.display = "inline-block";
 
       }
 
@@ -74,20 +66,14 @@ HTMLWidgets.widget({
 
     function reset_form(doc) {
 
-      var rcode = $qs("div#" + self.thel.id + " > div.rcodediv");
-      var msg = $qs("div#" + self.thel.id + " > div.msgdiv");
-      var xpath = $qs("div#" + self.thel.id + " > div.filterdiv > input.filterinput");
+      self.rcode.style.display = "none";
+      self.msg.style.display = "none";
+      self.xpath.value = "" ;
 
-      rcode.style.display = "none";
-      msg.style.display = "none";
-      xpath.value = "" ;
+      self.xml_div.innerText = vkbeautify.xml(self.glob.xmlDoc);
+      self.xml_div.textContent = vkbeautify.xml(self.glob.xmlDoc);
 
-      var xml_div = $qs("div#" + self.thel.id + " > pre > code.xmldiv");
-
-      xml_div.innerText = vkbeautify.xml(glob[self.thel.id].xmlDoc);
-      xml_div.textContent = vkbeautify.xml(glob[self.thel.id].xmlDoc);
-
-      hljs.highlightBlock(xml_div);
+      hljs.highlightBlock(self.xml_div);
 
       return(false);
 
@@ -95,14 +81,11 @@ HTMLWidgets.widget({
 
     function generate_rcode() {
 
-      var rcode = $qs("div#" + self.thel.id + " > div.rcodediv");
-      var xpath = $qs("div#" + self.thel.id + " > div.filterdiv > input.filterinput");
-
-      rcode.innerText = "xml2::xml_find_all(doc, '" +
-                               xpath.value + "', ns=xml2::xml_ns(doc))";
-      rcode.textContent = "xml2::xml_find_all(doc, '" +
-                                 xpath.value + "', ns=xml2::xml_ns(doc))";
-      rcode.style.display = "inline-block";
+      self.rcode.innerText = "xml2::xml_find_all(doc, '" +
+                               self.xpath.value + "', ns=xml2::xml_ns(doc))";
+      self.rcode.textContent = "xml2::xml_find_all(doc, '" +
+                                 self.xpath.value + "', ns=xml2::xml_ns(doc))";
+      self.rcode.style.display = "inline-block";
 
       return(false);
 
@@ -111,16 +94,17 @@ HTMLWidgets.widget({
     var serializer = new XMLSerializer();
     var domparser =  new DOMParser();
 
-    glob[el.id] = {};
-    glob[el.id].xmlDoc     = param.xmlDoc;
-    glob[el.id].applyXPath = param.applyXPath;
-    glob[el.id].orig       = param.xmlDoc;
-    glob[el.id].serialize  = serializer;
-    glob[el.id].parser     = domparser;
-    glob[el.id].parsedOrig = domparser.parseFromString(param.xmlDoc, "text/xml");
+    self.glob.xmlDoc     = param.xmlDoc;
+    self.glob.applyXPath = param.applyXPath;
+    self.glob.orig       = param.xmlDoc;
+    self.glob.serialize  = serializer;
+    self.glob.parser     = domparser;
+    self.glob.parsedOrig = domparser.parseFromString(param.xmlDoc,
+                           "text/xml");
 
     var link = document.createElement("link");
-    link.href  = "lib/highlightjs-9.0.0/styles/" + param.styleSheet + ".css";
+    link.href  = "lib/highlightjs-9.0.0/styles/" +
+                 param.styleSheet + ".css";
     link.type  = "text/css";
     link.rel   = "stylesheet";
     link.media = "screen,print";
@@ -152,15 +136,18 @@ HTMLWidgets.widget({
       $qs("div#" + el.id + " > div.filterdiv > button.rcodeclass").onclick = generate_rcode;
     }
 
-    var xml_div = $qs("div#" + el.id + " > pre > code.xmldiv");
+    self.xml_div = $qs("div#" + el.id + " > pre > code.xmldiv");
+    self.msg = $qs("div#" +  el.id + " > div.msgdiv");
+    self.xpath = $qs("div#" + el.id + " > div.filterdiv > input.filterinput");
+    self.rcode = $qs("div#" + el.id + " > div.rcodediv");
 
-    if (glob[el.id].applyXPath === null) {
+    if (self.glob.applyXPath === null) {
 
-      xml_div.innerText = vkbeautify.xml(param.xmlDoc);
-      xml_div.textContent = vkbeautify.xml(param.xmlDoc);
+      self.xml_div.innerText = vkbeautify.xml(param.xmlDoc);
+      self.xml_div.textContent = vkbeautify.xml(param.xmlDoc);
 
       hljs.initHighlighting() ;
-      hljs.highlightBlock(xml_div);
+      hljs.highlightBlock(self.xml_div);
 
     } else {
       if (param.addFilter) { $qs("div#" + el.id + " > div.filterdiv > input.filterinput").value = param.applyXPath }
